f24ad2b8e6aa5f903651f8646e5e9c49e1564d36
Lorene Pecci <lorenepecci@gmail.com>
2022-05-31 14:35:03 -0300

    logo squid e imagens da api carregadas

--- scripts.js
+++ scripts.js
@@ -11,6 +11,13 @@ const fetchAPI = async () => {
   }
 };
 
+function createProductImageElement(imageSquid) {
+  const img = document.createElement('img');
+  img.className = 'item_image';
+  img.src = imageSquid;
+  return img;
+}
+
 function createCustomElement(element, className, innerText) {
   const e = document.createElement(element);
   e.className = className;
@@ -18,14 +25,15 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
-function createProductItemElement({link, usuario, upvotes, comentarios, criadoEm}) {
+function createProductItemElement({link, usuario, upvotes, comentarios, criadoEm, img}) {
   const section = document.createElement('section');
   section.className = 'item';
   section.appendChild(createCustomElement('span', 'item_link', link));
   section.appendChild(createCustomElement('span', 'item_usuario', usuario));
   section.appendChild( createCustomElement( 'span', 'item_upvotes', upvotes ) );
   section.appendChild( createCustomElement( 'span', 'item_comentarios', comentarios ) );
-  section.appendChild(createCustomElement('span', 'item_criadoEm', criadoEm));
+  section.appendChild( createCustomElement( 'span', 'item_criadoEm', criadoEm ) );
+  section.appendChild(createProductImageElement(img))
   return section;
 }
 
@@ -35,9 +43,10 @@ async function addImages() {
   avisoCarregando.remove();
   const sectionItem = document.querySelector('.body-images');
   for (let index = 0; index < data.length; index += 1) {
-    const { link, usuario, upvotes, comentarios, criadoEm } = data[ index ];
+    const { link, usuario, upvotes, comentarios, criadoEm,imagens } = data[ index ];
+    const img = imagens.thumbnail.url;
     sectionItem.appendChild(
-      createProductItemElement({ link, usuario: usuario.username, upvotes, comentarios, criadoEm }),
+      createProductItemElement({ link, usuario: usuario.username, upvotes, comentarios, criadoEm, img }),
     );
   }
 }
